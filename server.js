102fbbff737efb4e97ffae3872ad87591e19d14b
niharika-1999 <niharika.rao@ymedialabs.com>
2021-10-11 19:55:22 +0530

    Added:Logger using Winston

--- server.js
+++ server.js
@@ -1,26 +1,25 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-
-// create express app
 const app = express();
-
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }))
-
-// parse requests of content-type - application/json
-app.use(bodyParser.json())
-
 // Configuring the database
 const dbConfig = require('./config/database.config.js');
 const mongoose = require('mongoose');
 
-mongoose.Promise = global.Promise;
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }))
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+
+//adding a logger
+const logger = require('./winstonLogger.js');
 
 // Connecting to the database
 mongoose.connect(dbConfig.url, {
     useNewUrlParser: true
 }).then(() => {
-    console.log("Successfully connected to the database");    
+    console.log("Successfully connected to the database");   
+    logger.info("Connected to Database"); 
 }).catch(err => {
     console.log('Could not connect to the database. Exiting now...', err);
     process.exit();
@@ -29,13 +28,14 @@ mongoose.connect(dbConfig.url, {
 // define a simple route
 app.get('/', (req, res) => {
     res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
+    logger.info("Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes.");
 });
 
 // Require Notes routes
 require('./app/routes/note.routes.js')(app);
 
-
 // listen for requests
 app.listen(3000, () => {
-    console.log("Server is listening on port 3000");
+    console.log("Server is listening on port 3000.");
+    logger.info("Server is listening on port 3000.");
 });
